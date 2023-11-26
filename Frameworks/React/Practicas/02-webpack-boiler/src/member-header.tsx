4e2b822d86ad9c6aa4f6681141acbe965bb71358
Lorena <lorena.rodriguez@tryptonsoftware.com>
2023-11-26 15:04:55 +0100

    Sesión 2 - React - Parte 1

---
+++ Frameworks/React/Practicas/02-webpack-boiler/src/member-header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MemberHeader = () => {
+    return (
+        <>
+          <span className="header">Avatar</span>
+          <span className="header">Id</span>
+          <span className="header">Login</span>
+        </>
+    )
+}
