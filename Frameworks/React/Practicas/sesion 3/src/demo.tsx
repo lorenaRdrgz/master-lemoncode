bebece97ae0131cbeaf7ed347f70bf522d4edba1
Lorena <lorena.rodriguez@tryptonsoftware.com>
2023-12-04 14:00:00 +0100

    React - Sesión 3 - Parte 2

--- Frameworks/React/Practicas/sesion 3/src/demo.tsx
+++ Frameworks/React/Practicas/sesion 3/src/demo.tsx
@@ -1,8 +1,9 @@
 import React, { PropsWithChildren } from "react";
 
 interface CounterContextModel {
-    countValue: number;
-    updateCount: (value: number) => void;
+    count: number;
+    increment: () => void;
+    decrement: () => void;
 }
 
 const CounterContext = React.createContext<CounterContextModel>(null);
@@ -10,27 +11,31 @@ const CounterContext = React.createContext<CounterContextModel>(null);
 const CounterProvider: React.FC<PropsWithChildren> = ({ children }) => {
     const [count, setCount] = React.useState(0);
 
-    const updateCount = (value: number) => {
-        if (value < 5) {
-            setCount(value);
-        }
-        else {
-            setCount(0);
-        }
-    }
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
 
     return <CounterContext.Provider value={{
-        countValue: count,
-        updateCount: updateCount
+        count: count,
+        increment: increment,
+        decrement:decrement
     }} >{children}</CounterContext.Provider>
 }
 
 const Increment: React.FC = () => {
-    const { countValue, updateCount } = React.useContext(CounterContext);
+    const { count, increment} = React.useContext(CounterContext);
     return (
         <>
-            <h1>Counter: {countValue}</h1>
-            <button onClick={() => updateCount(countValue + 1)}>Increment!</button>
+            <h1>Count: {count}</h1>
+            <button onClick={increment}>Increment!</button>
+        </>
+    )
+}
+
+const Decrement: React.FC = () => {
+    const { decrement} = React.useContext(CounterContext);
+    return (
+        <>
+            <button onClick={decrement}>Decrement!</button>
         </>
     )
 }
@@ -39,6 +44,7 @@ export const Demo: React.FC = () => {
     return (
         <CounterProvider>
             <Increment />
+            <Decrement />
         </CounterProvider>
     )
 }
