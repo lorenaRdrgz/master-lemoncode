7506d7e1570adb74155affbf41f50065024f4141
Lorena <lorena.rodriguez@tryptonsoftware.com>
2024-02-19 16:39:03 +0100

    Ejercicio React cambio router

--- Frameworks/React/Ejercicios/Basico/src/router/app.router.tsx
+++ Frameworks/React/Ejercicios/Basico/src/router/app.router.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import { MemberListScene, MemberDetailScene, CharacterListScene, CharacterDetailScene } from "@/scenes";
+import { MemberListScene, MemberDetailScene, CharacterListScene, CharacterDetailScene, LoginScene } from "@/scenes";
 import { switchRoutes } from "./routes";
 
 export const AppRouter = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path={switchRoutes.root} element={<MemberListScene />} />
+        <Route path={switchRoutes.root} element={<LoginScene />} />
+        <Route path={switchRoutes.list} element={<MemberListScene />} />
         <Route path={switchRoutes.detail} element={<MemberDetailScene />} />
         <Route path={switchRoutes.listCharacter} element={<CharacterListScene />} />
         <Route path={switchRoutes.detailCharacter} element={<CharacterDetailScene />} />
