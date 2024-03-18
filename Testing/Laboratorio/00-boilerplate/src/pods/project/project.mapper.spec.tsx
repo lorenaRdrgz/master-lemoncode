666e6058f595966ee592485403cc9347d3304bb9
Lorena <lorena.rodriguez@tryptonsoftware.com>
2024-03-18 18:17:10 +0100

    primera prueba test

---
+++ Testing/Laboratorio/00-boilerplate/src/pods/project/project.mapper.spec.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+describe('common/ProyectMapper', () => {
+
+    it ('should pass spec', () =>{
+        //Arrange
+
+        //Act
+
+        //Assert
+        expect(true).toBeTruthy();
+    });
+
+})
